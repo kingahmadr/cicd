f44834774c8eac44dcc3756bc444b2fd6e6e1a25
kingahmadr <ahmadnromiz@gmail.com>
2022-06-18 10:58:09 +0700

    revised on db init scripts

---
+++ database-build/02.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `database2`;
+GRANT ALL ON `test`.* TO 'user_db'@'%';
---
+++ database-build/03.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `database3`;
+GRANT ALL ON `test`.* TO 'user_db'@'%';
