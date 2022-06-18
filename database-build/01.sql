79f467c6864f0aabd289f2bc7a33292d7ed2d51d
kingahmadr <ahmadnromiz@gmail.com>
2022-06-18 10:41:10 +0700

    add some builds

---
+++ database-build/01.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS `test`;
+GRANT ALL ON `test`.* TO 'user_db'@'%';
+
+CREATE DATABASE IF NOT EXISTS `database2`;
+GRANT ALL ON `test`.* TO 'user_db'@'%';
+
+CREATE DATABASE IF NOT EXISTS `database3`;
+GRANT ALL ON `test`.* TO 'user_db'@'%';
