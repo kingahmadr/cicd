b4ab75fecbbfa9393339b052ecdff04a1e4f3631
kingahmadr <ahmadnromiz@gmail.com>
2022-06-18 11:09:39 +0700

    revised

--- database-build/01.sql
+++ database-build/01.sql
@@ -1,8 +1,6 @@
 CREATE DATABASE IF NOT EXISTS `test`;
 GRANT ALL ON `test`.* TO 'user_db'@'%';
-
 CREATE DATABASE IF NOT EXISTS `database2`;
 GRANT ALL ON `test`.* TO 'user_db'@'%';
-
 CREATE DATABASE IF NOT EXISTS `database3`;
 GRANT ALL ON `test`.* TO 'user_db'@'%';
